5eff24ea0c4f991ab3f578662bb65c2350bdafd5
camila breda <camilabbreda@gmail.com>
2025-10-28 14:31:41 -0300

    cron fix

--- src/cron/daily-notifications.ts
+++
@@ -1,9 +0,0 @@
-import cron from 'node-cron';
-import { processAllNotifications } from '../services/notification-service';
-
-// processAllNotifications()
-const task = cron.schedule('0 15 * * 1-5', processAllNotifications, {
-  timezone: 'America/Sao_Paulo'
-});
-
-task.start();
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 import { processAllNotifications } from './services/notification-service';
 dotenv.config();
 
-processAllNotifications()
 
 console.log("[Blip Notification] Cron job is active and will execute daily at 3:00 PM (America/Sao_Paulo).");
+// processAllNotifications()
 const task = cron.schedule('0 15 * * 1-5', processAllNotifications, {
   timezone: 'America/Sao_Paulo'
 });
