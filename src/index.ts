92d034b861e266139eae34384d72ba7c02ba934a
camila breda <camilabbreda@gmail.com>
2025-12-04 18:52:02 -0300

    remove not used files

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import cron from 'node-cron';
 import dotenv from 'dotenv';
-import { processAllNotifications } from './services/notification-service';
+// import { processAllNotifications } from './services/notification-service';
 import { processAllSankhyaNotifications } from './services/notification-sankhya-service';
 dotenv.config();
 
 
-console.log("[Cogni Notification] Cron job is active and will execute daily at 3:00 PM (America/Sao_Paulo).");
-// processAllNotifications()
-const task = cron.schedule('0 15 * * 1-5', processAllNotifications, {
-  timezone: 'America/Sao_Paulo'
-});
+// console.log("[Cogni Notification] Cron job is active and will execute daily at 3:00 PM (America/Sao_Paulo).");
+// // processAllNotifications()
+// const task = cron.schedule('0 15 * * 1-5', processAllNotifications, {
+//   timezone: 'America/Sao_Paulo'
+// });
 
-task.start();
+// task.start();
 
 console.log("[Sankhya Notification] Cron job is active and will execute daily at 3:00 PM (America/Sao_Paulo).");
 // processAllSankhyaNotifications()
